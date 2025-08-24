2d2f415c934d75125d3a884edb18adf0d4356ac1
Oleksiy Nikolaenko <aleshka.dev@gmail.com>
2025-08-24 16:01:13 +0300

    add Prisma module and service for database integration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
     imports: [
@@ -7,6 +8,7 @@ import { ConfigModule } from '@nestjs/config';
             isGlobal: true,
             envFilePath: ['.env'],
         }),
+        PrismaModule,
     ],
     controllers: [],
     providers: [],
